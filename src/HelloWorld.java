f308fc107bcc3d4a724a459a8734aa3d39eb385c
PaulaSoler000 <paulasoler111@gmail.com>
2024-08-21 13:20:20 +0200

    java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
 
 public class HelloWorld {
 
@@ -46,60 +47,60 @@ public class HelloWorld {
         myBoolean = null;
         System.out.println(myBoolean);
 
-        //constantes
-        System.out.println("++++++++Constantes+++++++");
-        myFloat = null;
-        if (myFloat != null) {
-            System.out.println(myFloat + 10);
-            System.out.println("My Float es distinto de nulo");
-        } else {
-            System.out.println("My Float es nulo");
-        }
-
-        if (myInt == 11) {
-            System.out.println("myInt == 11");
-        } else if (myInt == 12) {
-            System.out.println("myInt == 12");
-        } else {
-            System.out.println("El valor no es ni 11 ni 12");
-        }
-
-
-
-        // Listas
-        System.out.println("++++++++Listas+++++++");
-        List<String> myList = new ArrayList();
-        myList.add(myString);
-        myList.add(myInt.toString());
-        System.out.println(myList);
-
-
-        // Mapas
-        System.out.println("++++++++Mapas+++++++");
-        Map<String, String> myMap = new HashMap();
-        myMap.put("string", myString);
-        myMap.put("int", myInt.toString());
-        System.out.println(myMap);
-        System.out.println(myMap.get("int"));
-
-
-        // Bucles
-        System.out.println("++++++++Bucles+++++++");
-        for (int index = 0; index < myList.size(); index++) {
-            System.out.println(myList.get(index));
-        }
-
-        // Clases
-        System.out.println("++++++++Clases+++++++");
-        HelloWorld myMain = new HelloWorld();
-        System.out.println(myMain.myFunction(5, 2));
-        System.out.println(myMain.myFunction(3, 126389));
+//        //constantes
+//        System.out.println("++++++++Constantes+++++++");
+//        myFloat = null;
+//        if (myFloat != null) {
+//            System.out.println(myFloat + 10);
+//            System.out.println("My Float es distinto de nulo");
+//        } else {
+//            System.out.println("My Float es nulo");
+//        }
+//
+//        if (myInt == 11) {
+//            System.out.println("myInt == 11");
+//        } else if (myInt == 12) {
+//            System.out.println("myInt == 12");
+//        } else {
+//            System.out.println("El valor no es ni 11 ni 12");
+//        }
+//
+//
+//
+//        // Listas
+//        System.out.println("++++++++Listas+++++++");
+//        List<String> myList = new ArrayList();
+//        myList.add(myString);
+//        myList.add(myInt.toString());
+//        System.out.println(myList);
+//
+//
+//        // Mapas
+//        System.out.println("++++++++Mapas+++++++");
+//        Map<String, String> myMap = new HashMap();
+//        myMap.put("string", myString);
+//        myMap.put("int", myInt.toString());
+//        System.out.println(myMap);
+//        System.out.println(myMap.get("int"));
+//
+//
+//        // Bucles
+//        System.out.println("++++++++Bucles+++++++");
+//        for (int index = 0; index < myList.size(); index++) {
+//            System.out.println(myList.get(index));
+//        }
+//
+//        // Clases
+//        System.out.println("++++++++Clases+++++++");
+//        HelloWorld myMain = new HelloWorld();
+//        System.out.println(myMain.myFunction(5, 2));
+//        System.out.println(myMain.myFunction(3, 126389));
     }
 
     // Funciones
 
-    public int myFunction(int myFirstNumber, int mySecondNumber) {
-        return myFirstNumber + mySecondNumber;
+//    public int myFunction(int myFirstNumber, int mySecondNumber) {
+//        return myFirstNumber + mySecondNumber;
 
     }
-}
+//}
