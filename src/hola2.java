c815f5d2ca3841d735406758e66b319c6327febd
PaulaSoler000 <paulasoler111@gmail.com>
2024-09-05 21:02:15 +0200

    javita2

---
+++ src/hola2.java
@@ -0,0 +1,66 @@
+public class hola2 {
+    public static void main(String[] args) {
+
+        String myString = "una frase";
+        int myInt = 1;
+
+        myString = myString + myInt;
+
+//        System.out.println(myString);
+
+//        if(myInt==2){
+//            System.out.println(myString);
+//
+//        }else{
+//            System.out.println("valor no es 2");
+//        }
+
+//        for (int i =0; i<myString.length()+1; i++){
+//
+//            if( i==myString.length()) {
+//                System.out.println(i);
+//            }
+//        }
+// int edad de grogu = 50
+// cuantos años van a pasar hasta que grogu tenga 200
+// solo un bucle
+// imprimir solo una vez el resultado
+//        int edadGrogu=50;
+//        for (int i =0; edadGrogu<=200; i++){
+//            if( edadGrogu==200) {
+//                System.out.println(i);
+//            }
+//            edadGrogu++;
+//        }
+
+
+//        for (int i =1; i<=20; i++){
+//
+//            if(i%2==1){
+//                System.out.println(i);
+//            }
+//        }
+
+//      char letra = myString.charAt(5);
+//
+//        System.out.println(letra);
+//
+//        for (int i =0; i<myString.length(); i++){
+//
+//            if(myString.charAt(i)!='a'&& myString.charAt(i)!='e' && myString.charAt(i)!='i' && myString.charAt(i)!='o' && myString.charAt(i)!='u') {
+//                System.out.println(i);
+//
+//            }
+//        }
+
+        String nombre = "grogu";
+        if (nombre.equals("grogu")){
+            System.out.println("correcto");
+        }else{
+            System.out.println("incorrecto");
+        }
+
+
+
+    }
+}
