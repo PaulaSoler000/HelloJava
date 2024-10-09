3892eaebe9ad1680e5107f133b415295a954e847
PaulaSoler000 <93978384+PaulaSoler000@users.noreply.github.com>
2024-10-09 11:51:50 +0200

    treeset1

--- src/ejercicios_java/set2.java
+++ src/ejercicios_java/set2.java
@@ -1,4 +1,5 @@
 package ejercicios_java;
+
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,27 +13,43 @@ public class set2 {
         conjunto.add(100);
 
         System.out.println(conjunto);
-//        for (Integer i : conjunto) {
-//            System.out.println(i);
-//        }
 
-        System.out.println( conjunto.first());
-        System.out.println( conjunto.last());
+
+
+        System.out.println(conjunto.first());
+        System.out.println(conjunto.last());
 
 
         Scanner lectura = new Scanner(System.in);
         System.out.println("Introduce el primer texto");
-        Integer texto1 = lectura.nextInt();
+        Integer numero = lectura.nextInt();
 
-//        if(texto1){
-//
-//        }
+        conjunto.add(numero);
+
+        Integer numMayor = conjunto.higher(numero);
 
 
+        if (numMayor != null) {
 
+            System.out.println("el siguiente mas alto es: " + numMayor);
+
+        } else {
+            System.out.println("número no válido");
+        }
+
+        Integer numMenor = conjunto.lower(numero);
+
+        if (numMenor != null) {
+
+            System.out.println("el anterior mas bajo es: " + numMenor);
+
+        } else {
+            System.out.println("número no válido");
+        }
 
 
     }
 
 
 }
+
