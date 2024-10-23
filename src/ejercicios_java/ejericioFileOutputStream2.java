470178d5acc0ef71785992c84bc7e1d9fcdbf740
PaulaSoler000 <93978384+PaulaSoler000@users.noreply.github.com>
2024-10-23 10:36:20 +0200

    actividad FileOutputStream

--- src/ejercicios_java/ejericioFileOutputStream2.java
+++ src/ejercicios_java/ejericioFileOutputStream2.java
@@ -5,19 +5,45 @@ import java.util.Scanner;
 
 public class ejericioFileOutputStream2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        PrintWriter pw = new PrintWriter(new FileOutputStream(new File("data.bin")));
-        for (int i = 1; i <= 10; ++i)
-            pw.print(i);
+//        PrintWriter pw = new PrintWriter(new FileOutputStream(new File("data.bin")));
+//        for (int i = 1; i <= 10; ++i)
+//            pw.print(i);
+//
+//        pw.close();
+//
+//        FileReader fr = new FileReader("data.bin");
+//        int c;
+//        while ((c = fr.read()) != -1) {
+//            System.out.print(c);
+//        }
+//        fr.close();
 
-        pw.close();
 
-        FileReader fr = new FileReader("data.bin");
-        int c;
-        while ((c = fr.read()) != -1)
-            System.out.print((char) c);
-        fr.close();
+        File fr2 = new File("data2.bin");
 
+        try {
+            FileOutputStream fos = new FileOutputStream(fr2);
+            fos.write(0);
+            fos.write(1);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream(fr2);
+            try {
+                int num = fis.read();
+                while (num != -1) {
+                    System.out.print(num);
+                    num = fis.read();
+                }
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
